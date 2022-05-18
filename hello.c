5c72d1c069fc23d47874d16c8aea63ff01e57bc5
gaoloveqiao <3307560981@qq.com>
2022-05-18 11:18:15 +0800

    初始化项目版本‘

---
+++ hello.c
@@ -0,0 +1,12 @@
+//
+//  c1.c
+//  
+//
+//  Created by 高铖 on 2021/10/18.
+//
+#include <stdio.h>/*预处理器指令，告诉 C 编译器在实际编译之前要包含 stdio.h 文件*/
+int main(void){
+    /*主函数，程序从这里开始执行*/
+    printf("Hello World !\n");/*函数，在屏幕上显示消息 "Hello, World!"*/
+    return 0;/*终止 main() 函数，并返回值 0*/
+}
